17cf3efab4e3d1e76b2d851e5412088de9e3d699
vomelchuk <vitaliy.omelchuk@gmail.com>
2016-04-08 09:34:54 +0300

    testt

--- wallet.py
+++ wallet.py
@@ -1,9 +1,33 @@
 class Money:
-    def __init__(self, currency, amount):
-        self.currency = currency
+    type_currency = ["Usd", "Euro", "Hrn"]
+
+    def __init__(self, index_currency, amount):
+        self.currency = self.type_currency[index_currency]
         self.amount = amount
+
+    def add_money(self, index_currency, val):
+        self.amount += val
+        self.currency = self.type_currency[index_currency]
+
+    def __str__(self):
+        return "Wallet has " + str(self.amount) + self.currency
+        pass
 
 
 class Wallet:
+    tags = []
 
-    pass
+    def __init__(self):
+        self.money = Money(0, 0)
+
+    def put_money(self, amount):
+
+        pass
+
+    def take_money(self, amount):
+        pass
+
+
+
+
+
